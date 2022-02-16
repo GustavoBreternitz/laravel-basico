7afeb79530114277b7ff7142551c4fe07f6f5b03
Gustavo Breternitz <breternitzgustavo@gmail.com>
2022-02-16 19:25:55 -0300

    modularizando aplicação

--- src/config/app.php
+++ src/config/app.php
@@ -161,6 +161,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        Nwidart\Modules\LaravelModulesServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -229,6 +230,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Module' => Nwidart\Modules\Facades\Modules::class,
 
     ],
 
